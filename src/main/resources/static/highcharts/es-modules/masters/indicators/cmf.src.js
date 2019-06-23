9060a58f3ebd0b54cca1ec2ddcf2c31e0baece71
Rob Gregory [Minty] <MintyMods@users.noreply.github.com>
2019-06-23 22:37:36 +0100

    added demo content to prototype navigation

---
+++ src/main/resources/static/highcharts/es-modules/masters/indicators/cmf.src.js
@@ -0,0 +1,13 @@
+/**
+ * @license  @product.name@ JS v@product.version@ (@product.date@)
+ * @module highcharts/indicators/cmf
+ * @requires highcharts
+ * @requires highcharts/modules/stock
+ *
+ * (c) 2010-2019 Highsoft AS
+ * Author: Sebastian Domas
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../indicators/cmf.src.js';
---
+++ src/main/resources/static/highcharts/es-modules/masters/indicators/psar.src.js
@@ -0,0 +1,14 @@
+/**
+ * @license  @product.name@ JS v@product.version@ (@product.date@)
+ * @module highcharts/indicators/psar
+ * @requires highcharts
+ * @requires highcharts/modules/stock
+ *
+ * Parabolic SAR Indicator for Highstock
+ *
+ * (c) 2010-2019 Grzegorz Blachliński
+ *
+ * License: www.highcharts.com/license
+ */
+'use strict';
+import '../../indicators/psar.src.js';
